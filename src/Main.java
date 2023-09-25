b5dffc3cbd5c4557cf05ee22bfbf9e5fd8790973
Fishman <fishman.ksenia@gmail.com>
2023-09-25 12:35:47 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 20_521;
+
+        int cost = 20;
+
+        int mile = amount / cost;
+
+        System.out.println("miles you have - " + mile);
+
+    }
+}
